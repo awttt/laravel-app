cede7ed869edf1fe0a58c815f84da481d64f5a5a
went.an <went.an@hnair.com>
2023-06-25 00:08:21 +0800

    laravel init

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaticPagesController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [StaticPagesController::class, 'home']);
+Route::get('/help', [StaticPagesController::class, 'help']);
+Route::get('/about', [StaticPagesController::class, 'about']);
